d6f6564414afc85bacb78218eba71abe6a115c7a
William Whitehead <willclashtest@gmail.com>
2025-11-13 11:51:43 -0600

    Initial Commit

---
+++ ClipCache/ClipCacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipCacheApp.swift
+//  ClipCache
+//
+//  Created by Will Whitehead on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipCacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipCache/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClipCache
+//
+//  Created by Will Whitehead on 11/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
